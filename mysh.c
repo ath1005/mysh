4e06e80d2c6b2685e9904306ad79dd82d1982d3c
Atlee Thomas Hasson <ath1005@cs.rit.edu>
2019-04-18 11:28:41 -0400

    Added the history struct and functions back

--- mysh.c
+++ mysh.c
@@ -6,18 +6,57 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct history_s{
+	int total;
+	char ** cmdHistory;
+	int length;
+};
+
+typedef struct history_s *history;
+
+history create_history(int length){
+	history h = NULL;
+
+	h = malloc(sizeof(struct history_s));
+
+	h->cmdHistory = malloc(length * sizeof(char*));
+
+	for(int i = 0; i < length; i++){
+		h->cmdHistory[i] = malloc(256 * sizeof(char*));
+	}
+
+	h->length = length;
+
+	h->total = 0;
+
+	return h;
+}
+
+void destroy_history(history h){
+	for(int i = 0; i < h->length; i++){
+		free(h->cmdHistory[i]);
+	}
+	
+	free(h->cmdHistory);
+	free(h);
+}
+
 void prompt(int counter){
 	printf("mysh[%d]> ", counter);	
 }
 
 int main(int argc, char *argv[]){
 	char *buffer;
-	int count = 0;
+	history h = create_history(10);
 	size_t bufsize = 256;
 
 	buffer = malloc(bufsize * sizeof(char));
-	prompt(count);
+	prompt(h->total);
 	getline(&buffer, &bufsize, stdin);
+
+	h->cmdHistory[h->total] = strtok(buffer, "\n");
+
+	printf("%s\n", h->cmdHistory[0]);
 
 	char * command = strtok(buffer, " \n");
 
@@ -30,12 +69,14 @@ int main(int argc, char *argv[]){
 			token = strtok(NULL, " \n");
 		}
 		
-		count++;
-		prompt(count);
+		h->total++;
+		prompt(h->total);
 		getline(&buffer, &bufsize, stdin);
+		h->cmdHistory[h->total] = strtok(buffer, "\n");
 		command = strtok(buffer, " \n");
 	}
 	
+	destroy_history(h);
 	free(buffer);	
 	return EXIT_SUCCESS;
 }
