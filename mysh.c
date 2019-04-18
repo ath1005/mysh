9fdab203c952651a54321fb71824ddeb58a93345
Atlee Thomas Hasson <ath1005@cs.rit.edu>
2019-04-17 20:38:15 -0400

    Added a simple prompt function so that the program echoes the first input word

--- mysh.c
+++ mysh.c
@@ -6,16 +6,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+int prompt(int counter){
+	printf("mysh[%d]> ", counter);
+	counter++;
+	return counter;	
+}
+
 int main(int argc, char *argv[]){
 	char *buffer;
 	size_t bufsize = 256;
 
 	buffer = malloc(bufsize * sizeof(char));
-	//prompt
+	prompt(0);
 	getline(&buffer, &bufsize, stdin);
 
 	char * command = strtok(buffer, " \n");
 	printf("%s\n", command);
-	
+
+	free(buffer);	
 	return main(argc, argv);
 }
