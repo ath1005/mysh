e6c4843b3c370a12e8a075273c0adc4f8d3e46a3
Atlee Thomas Hasson <ath1005@cs.rit.edu>
2019-04-19 16:26:57 -0400

    Created a new function to handle the execution of commands outside the main method

--- mysh.c
+++ mysh.c
@@ -10,7 +10,7 @@ void prompt(int counter){
 	printf("mysh[%d]> ", counter);	
 }
 
-int history_cmd(int argc, char * argv[], int length, char *history[length], int current){
+int history_cmd(int argc, char * argv[], int length, char * history[], int current){
 	int index = current % length;
 
 	if(current < length){
@@ -29,11 +29,60 @@ int history_cmd(int argc, char * argv[], int length, char *history[length], int 
 
 int help_cmd(int argc, char * argv[]){
 	printf("Internal commands:\n");
-	printf("Bang command: !N\t\t\ttRe-execute the Nth command in the hisroty list\n");
+	printf("Bang command: !N\t\t\tRe-execute the Nth command in the hisroty list\n");
 	printf("Help command: help\t\t\tPrint this help menu\n");
 	printf("History command: history\t\tPrint a list of past commands and their arguments\n");
 	printf("Quit command: quit\t\t\tClean up memory and gracefully terminate the shell\n");
 	printf("Verbose command: verbose on | off\tPrint the commands and the results of major operations during their execution\n");	
+}
+
+int bang_cmd(int argc, char * argv[], int command, char * history[]){
+	char * target = history[command];
+}
+
+int execute_command(int argc, char * argv[], int count, char * history[], int length){
+	char * token;
+	char * command;
+	size_t bufsize = 256;
+	char * buffer = malloc(bufsize * sizeof(char));
+
+	prompt(count);
+        getline(&buffer, &bufsize, stdin);
+        free(history[count % 10]);
+
+	if(strtok(buffer, "\n") == NULL){
+        	printf("test\n");
+       		return 1;
+        }
+        history[count % 10] = strdup(strtok(buffer, "\n"));
+	count++;
+
+	command = strtok(buffer, " \n");
+	if(strcmp(command, "quit") == 0){
+		return 1;
+	}
+
+	while(token != NULL){
+		token = strtok(NULL, " \n");
+	}
+
+	if(strcmp(command, "history") == 0){
+		history_cmd(argc, argv, length, history, count);
+	}
+	else if(command[0] == '!'){
+		for(int i = 0; i < strlen(command); i++){
+			command[i] = command[i+1];
+		}
+		int num = strtol(command, NULL, 10);
+		printf("%d\n", num);
+	}
+	else if(strcmp(command, "help") == 0){
+		help_cmd(argc, argv);
+	}
+	else if(strcmp(command, "verbose") == 0){
+		//Verbose command
+	}
+	free(buffer);
 }
 
 int main(int argc, char *argv[]){
@@ -70,14 +119,19 @@ int main(int argc, char *argv[]){
 	}
 
 	int count = 0;
-	size_t bufsize = 256;
+	//size_t bufsize = 256;
 
-	buffer = malloc(bufsize * sizeof(char));
+	//buffer = malloc(bufsize * sizeof(char));
 
-	char * command;
-	char * token;
+	//char * command;
+	//char * token;
 
 	while(1){
+		if(execute_command(argc, argv, count, history, length) == 1){
+			break;
+		}
+		count++;
+		/**
 		prompt(count);
 		getline(&buffer, &bufsize, stdin);
 		free(history[count % 10]);
@@ -96,14 +150,17 @@ int main(int argc, char *argv[]){
 
 		while(token != NULL){
 			token = strtok(NULL, " \n");
-			//printf("%s\n", token);
 		}
 		
 		if(strcmp(command, "history") == 0){
 			history_cmd(argc, argv, length, history, count);
 		}
-		else if(strcmp(command, "!") == 0){
-			//Bang command
+		else if(command[0] == '!'){
+			for(int i = 0; i < strlen(command); i++){
+				command[i] = command[i+1];
+			}
+			int num = strtol(command, NULL, 10);
+			printf("%d\n", num);
 		}
 		else if(strcmp(command, "help") == 0){
 			help_cmd(argc, argv);
@@ -111,10 +168,9 @@ int main(int argc, char *argv[]){
 		else if(strcmp(command, "verbose") == 0){
 			//Verbose command
 		}
+		*/
 	}
 
-	history_cmd(argc, argv, length, history, count);
-	
-	free(buffer);	
+	//free(buffer);	
 	return EXIT_SUCCESS;
 }
